93936d0f4a02c478795487ec080bf70f942e66b1
juaznerzz <juaznerzzcop@gmail.com>
2025-06-04 13:59:49 -0500

    Add files via upload

---
+++ environment.js
@@ -0,0 +1,9 @@
+constante whatsappNumber = "573195999136" ;
+// URL base para la API de validación de licencia alojada en Render
+localStorage .setItem ( "URL" , " https://api-licenciasz-1.onrender.com" );
+// Redirigir a los usuarios para obtener ayuda al desinstalar la extensión
+localStorage.setItem (
+​​
+  "URL_REDIRECCIÓN" ,
+  `https://wa.me/${whatsappNumber}?text=Hola, desinstalé la extension por error`
+);
